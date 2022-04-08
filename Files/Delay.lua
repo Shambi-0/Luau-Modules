3c2337cf028f273a16d8ce949efb7a7250f5fc7a
Shambi <87273502+Shambi-0@users.noreply.github.com>
2022-04-08 12:13:55 -0500

    Update Delay.lua

--- Files/Delay.lua
+++ Files/Delay.lua
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (C) 2021, Luc Rodriguez (Aliases : Shambi, StyledDev).
+Copyright (C) 2022, Luc Rodriguez (Aliases : Shambi, StyledDev).
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
